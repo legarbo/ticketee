a0116606d5ca24a6fdd2835b19b1be83243334dc
Leopoldo Garcia <legarbo@gmail.com>
2012-06-07 12:32:23 -0430

    Implemented creating tickets for a project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Ticketee::Application.routes.draw do
-	resources :projects
+	resources :projects do
+		resources :tickets
+	end
 	root :to => "projects#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
