a6f9fc8c517028004edbe216d2e2ea9296e1d48b
Leopoldo Garcia <legarbo@gmail.com>
2012-06-12 11:46:14 -0430

    reload app from github

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'dynamic_form'
+
+gem 'jquery-rails'
+
+group :test, :development do
+  gem 'rspec-rails', '~> 2.5'
+end
+
+group :test do
+ gem 'cucumber-rails', '1.0.6'
+ gem 'capybara'
+ gem 'database_cleaner'
+ gem 'factory_girl'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'a62376f7e4dcad65e8dcdbc13a60ebcc8637bc392610f1b2368cd0de9dd4521465d2ecebd4fd781ae1876ff84c0a2bd467aad9e99d5210844ad965581932429d'
